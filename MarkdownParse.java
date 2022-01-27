ea3a13ca973e71175d1dc17a309484adb539efc3
winniecibo <winniechen.cibo@gmail.com>
2022-01-26 19:44:23 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,11 +12,17 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+
+            if (nextOpenBracket == -1) {
+                return toReturn;
+            }
+
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            // System.out.println(currentIndex);
         }
         return toReturn;
     }
